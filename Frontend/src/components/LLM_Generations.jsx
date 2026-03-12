7205a7a0c13a4feb5e5fcf9ab3cc35cc20f0b7f5
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2026-03-13 00:32:49 +0530

    Update LLM_Generations.jsx

--- Frontend/src/components/LLM_Generations.jsx
+++ Frontend/src/components/LLM_Generations.jsx
@@ -30,7 +30,7 @@ function formatMessagesForProvider(messages, provider) {
 const models = [
   "gemini-2.5-flash",
   // "gemini-2.5-pro",
-  "gemini-2.0-flash",
+  // "gemini-2.0-flash",
   // "gemini-1.5-flash",
   // "gemini-1.5-pro"
 ];
